8937bdc2c9320e5d93173129fc1b9c471573366c
lucasmillanm <70648632+lucasmillanm@users.noreply.github.com>
2023-03-27 16:51:59 +0200

    init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import { Parallax } from 'react-parallax';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import logo from './logo.svg';
+import jetpackbg from './pictures/jetpackbro.png'
 import './App.css';
 
 function App() {
+  // initialize AOS library
+  React.useEffect(() => {
+    AOS.init();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <Parallax bgImage={jetpackbg} strength={500}>
+          <Container>
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h1 data-aos="fade-up" data-aos-duration="1000">
+                Super Crazy Baby
+              </h1>
+            </header>
+          </Container>
+        </Parallax>
+      </div>
   );
 }
 
